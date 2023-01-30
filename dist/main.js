b3196bb553a5bd252f5967e6dd2299eff61800f9
TyphooN48 <andrew.burlackov@yandex.ru>
2023-01-30 17:47:53 +0300

    add dist

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{var t={743:function(t){t.exports=function(){"use strict";var t={};return t.default={info:function(){const t=new Map;return t.set("name","ajs"),t.set("version","1.0.0"),t}},t.default}()}},n={};function r(e){var o=n[e];if(void 0!==o)return o.exports;var s=n[e]={exports:{}};return t[e].call(s.exports,s,s.exports,r),s.exports}(()=>{const t=r(743);console.log(t.info())})()})();
